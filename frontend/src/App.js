03de97d49f1ead7b672c5674f9909da13bac4c21
AldrinAnaahat <aldrinanaahat@gmail.com>
2024-04-19 17:10:32 +0530

    footer jsx file added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
-import Footer from './components/footer';
+import Footer from './components/Footer';
+
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Header/>
       <main>
         <Outlet/>
