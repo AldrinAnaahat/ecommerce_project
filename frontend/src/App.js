9721a1b8da5a077699aa118a91b9471fd5c381f8
AldrinAnaahat <aldrinanaahat@gmail.com>
2024-04-19 17:21:50 +0530

    assets added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import { Outlet } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
-import Footer from './components/Footer';
+import Footer from './components/footer'
 
 
 function App() {
