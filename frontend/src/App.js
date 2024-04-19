007cbbcbdf34d7f64d737b14924700112dc55540
AldrinAnaahat <aldrinanaahat@gmail.com>
2024-04-19 17:05:34 +0530

    basic routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,16 @@
+import { Outlet } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/footer';
 
 function App() {
   return (
     <div className="App">
-       <p className='text-3xl'>Mern App</p>
+      <Header/>
+      <main>
+        <Outlet/>
+      </main>
+      <Footer/>
     </div>
   );
 }
